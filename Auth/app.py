b3c1c3f767a1a1219211a387efd3ec59248e2398
Uday-sidagana <uday.sidgana@gmail.com>
2025-02-22 03:38:12 +0530

    registration and login routes

--- Auth/app.py
+++ Auth/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'hi'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method =="POST":
+        #handle resistration
+        pass
+    return render_template('register.html')
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method== "POST":
+        pass
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
