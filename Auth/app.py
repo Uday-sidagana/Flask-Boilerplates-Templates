36bd65842e7763aa56e9ff4a13c46c2fddf7813e
Uday-sidagana <uday.sidgana@gmail.com>
2025-02-22 23:10:29 +0530

    __init__ error solved

--- Auth/app.py
+++ Auth/app.py
@@ -15,7 +15,8 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
 
-    def __init__(self, email,password):
+    def __init__(self, email,password, name):
+        self.name = name
         self.email = email
         self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
